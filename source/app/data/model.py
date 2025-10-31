f6ae7404400f3e1222d9733264784e6f511fcd37
dan <daniel@rocksoliddata.solutions>
2025-10-31 01:14:43 -0600

    adding service appointment model

--- source/app/data/model.py
+++ source/app/data/model.py
@@ -29,3 +29,11 @@ class TrainingSession(BaseModel):
     price = DecimalField(max_digits=10, decimal_places=2)
     teacher = ForeignKeyField(Teacher, backref='training_sessions')
 
+class ServiceAppointment(BaseModel):
+    service_apt_id = AutoField(primary_key=True)
+    name = CharField(max_length=45)
+    phone_number = CharField(max_length=15)
+    email = CharField(max_length=45)
+    appointment_date = DateField()
+    appointment_time = TimeField()
+    notes = CharField(max_length=255)
